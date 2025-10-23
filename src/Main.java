6a2bdd214d40fd4a0b1ce3eadb25333864217fcd
LuCh-Ans <luch2603@gmail.com>
2025-10-23 23:32:58 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,14 @@ import java.util.*;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
+//Транзакция(положить на счет/снять со счета) срдержит информацию о ее типе, сумме, времени и id
 class Transaction {
-    private static int nextId = 1;
+    private static int nextId = 1;//общий счетчик транзакций
     private int id;
     private String type;
     private double amount;
     private LocalDateTime timestamp;
-    private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+    private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");//для вывода времени в красивом формате
 
     public Transaction(String type, double amount) {
         this.id = nextId++;
@@ -23,6 +24,7 @@ class Transaction {
     public String getTime() { return timestamp.format(formatter); }
 }
 
+//Банковский счет, содержит информацию о владельце, балансе и истории операций
 class BankAccount {
     private String accNumber;
     private String username;
@@ -30,7 +32,7 @@ class BankAccount {
     private double balance;
     private List<Transaction> trans;
     private boolean isActive;
-
+    
     public BankAccount(String accNumber, String username, String password) {
         this.accNumber = accNumber;
         this.username = username;
@@ -39,11 +41,11 @@ class BankAccount {
         this.trans = new ArrayList<>();
         this.isActive = true;
     }
-
+    //корректность введенного пароля
     public boolean checkPassword(String inputPassword) {
         return this.password.equals(inputPassword);
     }
-
+    //положить деньги на счет
     public void deposit(double amount) {
         if (!isActive) throw new RuntimeException("Account is not active");
         if (amount <= 0) throw new RuntimeException("Amount must be positive");
@@ -52,7 +54,7 @@ class BankAccount {
         trans.add(new Transaction("DEPOSIT", amount));
         System.out.printf("Deposited: $%.2f\n", amount);
     }
-
+    //снять деньги со счета
     public void withdraw(double amount) {
         if (!isActive) throw new RuntimeException("Account is not active");
         if (amount <= 0) throw new RuntimeException("Amount must be positive");
@@ -62,12 +64,12 @@ class BankAccount {
         trans.add(new Transaction("WITHDRAWAL", amount));
         System.out.printf("Withdrawn: $%.2f\n", amount);
     }
-
+    //смотреть баланс
     public void showBalance() {
         if (!isActive) throw new RuntimeException("Account is not active");
         System.out.printf("\n balance: $%.2f\n", balance);
     }
-
+    //история операций
     public void transHist() {
         if (!isActive) throw new RuntimeException("Account is not active");
 
@@ -82,7 +84,7 @@ class BankAccount {
             System.out.print("\n");
         }
     }
-
+    //поиск по транзакциям
     public void searchTrans(String type, Double minAmount, Double maxAmount) {
         if (!isActive) throw new RuntimeException("Account not active");
 
@@ -106,16 +108,16 @@ class BankAccount {
             }
         }
     }
-
+    //геттеры для привятных полей: номер счета, имя владельца и баланс
     public String getAccNum() { return accNumber; }
     public String getUsername() { return username; }
     public double getBalance() { return balance; }
 }
-
+//Банковская система, открывается в начале
 class BankingSystem {
     private static BankAccount curAcc = null;
-    private static Scanner scanner = new Scanner(System.in);
-    private static Map<String, BankAccount> accounts = new HashMap<>();
+    private static Scanner scanner = new Scanner(System.in);//сканирует ввод пользователя
+    private static Map<String, BankAccount> accounts = new HashMap<>();//хранит все счета: номер счета-объект счета
 
     public static void main(String[] args) {
         System.out.print("BANK SYSTEM\n");
@@ -124,7 +126,7 @@ class BankingSystem {
             try {
                 mainMenu();
                 System.out.print("Select option: ");
-                String input = scanner.nextLine().trim();
+                String input = scanner.nextLine().trim();//читаем ввод и убираем лишние пробелы
 
                 if (input.isEmpty()) {
                     System.out.print("Please enter a number\n");
@@ -132,7 +134,7 @@ class BankingSystem {
                 }
 
                 int choice = Integer.parseInt(input);
-
+                //выбираем в меню доступные опции
                 switch (choice) {
                     case 1:
                         createNewAcc();
@@ -144,10 +146,10 @@ class BankingSystem {
                         allAcc();
                         break;
                     case 0:
-                        System.out.print("Goodbye!\n");
+                        System.out.print("Goodbye!\n");//при выходе
                         return;
                     default:
-                        System.out.print("Invalid option\n");
+                        System.out.print("Invalid option\n");//если неверный ввод
                 }
             } catch (Exception e) {
                 System.out.print("Error: " + e.getMessage()+"\n");
@@ -155,14 +157,14 @@ class BankingSystem {
             System.out.print("\n");
         }
     }
-
+    //опции
     private static void mainMenu() {
         System.out.print("1. Create account\n");
         System.out.print("2. Login\n");
         System.out.print("3. View all accounts\n");
         System.out.print("0. Exit\n");
     }
-
+    //меню при входе в аккаунт
     private static void accMenu() {
         System.out.print("\nACCOUNT MENU\n");
         System.out.print("User: " + curAcc.getUsername()+"\n");
@@ -174,11 +176,12 @@ class BankingSystem {
         System.out.print("5. Search transactions\n");
         System.out.print("6. Logout\n");
     }
+    //определяет, является ли сумма положительной
     private static double isPos() {
         while (true){
             try {
                 String input = scanner.nextLine().trim();
-                double value = Double.parseDouble(input.replace(',', '.'));
+                double value = Double.parseDouble(input.replace(',', '.'));//читаем сумму и с запятой, и с точкой
                 if (value <= 0) {
                     System.out.print("\nAmount must be positive");
                     continue;
@@ -190,21 +193,22 @@ class BankingSystem {
             }
         }
     }
-
+    //определяет, является ли число положительным, либо пустой ввод
     private static Double isPosOrNull() {
         String input = scanner.nextLine().trim();
         if (input.isEmpty()) return null;
         try {
-            return Double.parseDouble(input.replace(',', '.'));
+            return Double.parseDouble(input.replace(',', '.'));//читаем сумму и с запятой, и с точкой
         }
         catch (NumberFormatException e) {
             System.out.print("\nInvalid number");
             return null;
         }
     }
+    //создать новый аккаунт
     private static void createNewAcc() {
         System.out.print("\nCREATE NEW ACCOUNT\n");
-
+        //читаем имя пользователя, пока не будет непустой ввод
         String username;
         while (true) {
             System.out.print("Enter your name: ");
@@ -212,7 +216,7 @@ class BankingSystem {
             if (!username.isEmpty()) break;
             System.out.print("Name can not be empty\n");
         }
-
+        //просим ввести пароль, пока не будет непустой ввод
         String password;
         while (true) {
             System.out.print("Enter your password: ");
@@ -220,24 +224,24 @@ class BankingSystem {
             if (!password.isEmpty()) break;
             System.out.print("Password can not be empty\n");
         }
-
+        //подтверждение пароля, проверка на их мэтч
         System.out.print("Confirm password: ");
         String password2 = scanner.nextLine().trim();
         if (!password2.equals(password)) {
             System.out.print("Passwords are different\n");
             return;
         }
-
-        String accNum = numberGenerator();
-        curAcc = new BankAccount(accNum, username, password);
-        accounts.put(accNum, curAcc);
+        
+        String accNum = numberGenerator();//генерируем счет
+        curAcc = new BankAccount(accNum, username, password);//создаем новый объект
+        accounts.put(accNum, curAcc);//сохраняем немер счета и объект
 
         System.out.print("\nAccount created successfully!\n");
         System.out.print("Account number: " + accNum+"\n");
         System.out.print("Owner: " + username+"\n");
         accSession();
     }
-
+    //для генерации десятизначного номера
     private static String numberGenerator() {
         Random random = new Random();
         String number = "";
@@ -247,10 +251,10 @@ class BankingSystem {
         }
         return number;
     }
-
+    //вход в аккаунт
     private static void logToAcc() {
         System.out.print("\nLogin");
-
+        
         if (accounts.isEmpty()) {
             System.out.print("No accounts exist yet\n");
             return;
@@ -261,13 +265,13 @@ class BankingSystem {
 
         System.out.print("\nPassword: ");
         String password = scanner.nextLine().trim();
-
+        //поиск аккаунта по номеру
         BankAccount account = accounts.get(accNum);
         if (account == null) {
             System.out.print("\nAccount not found");
             return;
         }
-
+        //проверка пароля
         if (!account.checkPassword(password)) {
             System.out.print("\nWrong password");
             return;
@@ -277,58 +281,58 @@ class BankingSystem {
         System.out.print("\nWelcome back, " + curAcc.getUsername() + "!\n");
         accSession();
     }
-
+    //выводит список всех аккаунтов
     private static void allAcc() {
         System.out.print("\nAll accounts:\n");
         if (accounts.isEmpty()) {
             System.out.print("\nNo accounts\n");
             return;
         }
-
+        //выводит информацию о счете в виде: номер | имя | баланс
         for (BankAccount acc : accounts.values()) {
             System.out.print(acc.getAccNum() + " | " + acc.getUsername() + " | $" + acc.getBalance()+"\n");
         }
     }
-
+    //работа после авторизации 
     private static void accSession() {
         while (true) {
             try {
                 accMenu();
                 System.out.print("\nChoose action: ");
                 String input = scanner.nextLine().trim();
-
+                //если пустой ввод
                 if (input.isEmpty()) {
                     System.out.print("Please select an option\n");
                     continue;
                 }
 
                 int choice = Integer.parseInt(input);
-
+                //выбор операции
                 switch (choice) {
-                    case 1:
+                    case 1://пополнение
                         System.out.print("\nEnter deposit amount: $");
                         double dep = isPos();
                         curAcc.deposit(dep);
                         break;
-                    case 2:
+                    case 2://снятие
                         System.out.print("\nEnter withdrawal amount: $");
                         double wthd = isPos();
                         curAcc.withdraw(wthd);
                         break;
-                    case 3:
+                    case 3://баланс
                         curAcc.showBalance();
                         break;
-                    case 4:
+                    case 4://история операций
                         curAcc.transHist();
                         break;
-                    case 5:
+                    case 5://поиск по транзакциям
                         search();
                         break;
-                    case 6:
+                    case 6://выход из аккаунта
                         System.out.print("See you soon," + curAcc.getUsername() +"!\n");
                         curAcc = null;
                         return;
-                    default:
+                    default://если неверный ввод
                         System.out.print("Invalid choice\n");
                 }
             } catch (Exception e) {
@@ -336,18 +340,18 @@ class BankingSystem {
             }
         }
     }
-
+    //поиск по транзакциям
     private static void search() {
-        System.out.print("\nSearch transactions:\nType (1-deposit, 2-withdrawal, enter-skip):");
+        System.out.print("\nSearch transactions:\nType (1-deposit, 2-withdrawal, enter-skip):");//1-пополнение счета, 2-снятие со счета, ничего не введено-все операции
         String typeInput = scanner.nextLine().trim();
         String type = null;
         if (typeInput.equals("1")) type = "DEPOSIT";
         else if (typeInput.equals("2")) type = "WITHDRAWAL";
-        System.out.print("\nMin amount $: ");
+        System.out.print("\nMin amount $: ");//минимальный порог, можно не задавать
         Double minAmount = isPosOrNull();
-        System.out.print("\nMax amount $: ");
+        System.out.print("\nMax amount $: ");//максимальный порог, можно не задавать
         Double maxAmount = isPosOrNull();
-
+        //поиск по параметрам
         curAcc.searchTrans(type, minAmount, maxAmount);
     }
 }
